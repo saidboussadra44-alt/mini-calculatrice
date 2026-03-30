22610f7f51ae1ba2fcd78c7278316dcaf0b682d8
saidboussadra44-alt <saidboussadra44@gmail.com>
2026-03-30 16:03:45 +0100

    happy ci yml

--- mini-calculatrice/app.py
+++ mini-calculatrice/app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
@@ -16,9 +13,8 @@ def calculate():
     try:
         result = eval(expression)
         return jsonify({'result': result})
-    except Exception:
+    except:
         return jsonify({'result': 'Error'})
-
 
 if __name__ == '__main__':
     app.run(debug=True)
